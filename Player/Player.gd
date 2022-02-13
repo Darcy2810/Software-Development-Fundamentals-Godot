778810f7eb9174a594fd9c012e705b8018da9d82
darcy <0852201@schoolsnet.act.edu.au>
2022-02-14 10:32:19 +1100

    player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,10 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var movement_speed = 200
 func _ready():
-	pass # Replace with function body.
+ set_physics_process(true)
+
+ func _physics_process(delta)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
